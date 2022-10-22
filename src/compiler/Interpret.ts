434b933f49da81ee58b0efb4a62512eb4a9f6510
Jeffrey Zhang <jeffreyzhang2002@gmail.com>
2022-10-22 14:00:56 -0400

    branching

--- src/compiler/Interpret.ts
+++ src/compiler/Interpret.ts
@@ -2,19 +2,24 @@ import { ProgramState } from "../layout/App";
 
 function varCommand(nextState: ProgramState, state: ProgramState, tokens: string[]): ProgramState {
 
-    if(tokens.length != 3) {
+    if(tokens.length !== 3) {
         nextState.error = true;
         nextState.errorCode = "Invalid Parameter Count"
         return nextState;
     }
 
-    nextState.state[tokens[1]] = tokens[2];
+    if(!isNaN(parseInt(tokens[2])))
+        nextState.state[tokens[1]] = parseInt(tokens[2]);
+    else
+        nextState.state[tokens[1]] = tokens[2];
     return nextState ;
 }
 
 function addCommand(nextState: ProgramState, state: ProgramState, tokens: string[]) : ProgramState {
-    if(tokens.length === 4 && state.state[tokens[1]] && state.state[tokens[2]] && state.state[tokens[3]])
+    //console.log("ADD HERE:", tokens.length === 4, " ",  state.state[tokens[1]] != undefined, " ", state.state[tokens[2]], " ", state.state[tokens[3]]);
+    if(tokens.length === 4 && state.state[tokens[1]] !== undefined && state.state[tokens[2]] !== undefined && state.state[tokens[3]])
     {
+       
 
         if(!isNaN(parseInt(state.state[tokens[2]])) && !isNaN(parseInt(state.state[tokens[3]]))) {
             nextState.state[tokens[1]] = parseInt(state.state[tokens[2]]) + parseInt(state.state[tokens[3]]);
@@ -28,16 +33,15 @@ function addCommand(nextState: ProgramState, state: ProgramState, tokens: string
     nextState.error = true;
     nextState.errorCode = "Invalid Parameter"
     return nextState;
-
-
 }
 
 function subCommand(nextState: ProgramState, state: ProgramState, tokens: string[]) : ProgramState {
-    if(tokens.length === 4 && state.state[tokens[1]] && state.state[tokens[2]] && state.state[tokens[3]])
+    if(tokens.length === 4 && state.state[tokens[1]] !== undefined && state.state[tokens[2]] !== undefined && state.state[tokens[3]])
     {
 
         if(!isNaN(parseInt(state.state[tokens[2]])) && !isNaN(parseInt(state.state[tokens[3]]))) {
-            nextState.state[tokens[1]] = parseInt(state.state[tokens[2]]) - parseInt(state.state[tokens[3]]);
+            nextState.error = true;
+            nextState.errorCode = "Must be numbers"
             return nextState;
         }
 
@@ -48,14 +52,171 @@ function subCommand(nextState: ProgramState, state: ProgramState, tokens: string
     nextState.error = true;
     nextState.errorCode = "Invalid Parameter"
     return nextState;
-
-
 }
+
+function multCommand(nextState: ProgramState, state: ProgramState, tokens: string[]) : ProgramState {
+    if(tokens.length === 4 && state.state[tokens[1]] !== undefined && state.state[tokens[2]] !== undefined && state.state[tokens[3]])
+    {
+
+        if(!isNaN(parseInt(state.state[tokens[2]])) && !isNaN(parseInt(state.state[tokens[3]]))) {
+            nextState.error = true;
+            nextState.errorCode = "Must be numbers"
+            return nextState;
+        }
+
+        nextState.state[tokens[1]] = state.state[tokens[2]] * state.state[tokens[3]];
+        return nextState;
+    }
+
+    nextState.error = true;
+    nextState.errorCode = "Invalid Parameter"
+    return nextState;
+}
+
+function divCommand(nextState: ProgramState, state: ProgramState, tokens: string[]) : ProgramState {
+    if(tokens.length === 4 && state.state[tokens[1]] !== undefined && state.state[tokens[2]] !== undefined && state.state[tokens[3]])
+    {
+
+        if(!isNaN(parseInt(state.state[tokens[2]])) && !isNaN(parseInt(state.state[tokens[3]]))) {
+            nextState.error = true;
+            nextState.errorCode = "Must be numbers"
+            return nextState;
+        }
+
+        nextState.state[tokens[1]] = state.state[tokens[2]] / state.state[tokens[3]];
+        return nextState;
+    }
+
+    nextState.error = true;
+    nextState.errorCode = "Invalid Parameter"
+    return nextState;
+}
+
+function GOTOCommand(nextState: ProgramState, state: ProgramState, tokens: string[]) : ProgramState {
+    if(tokens.length === 2 && !isNaN(parseInt(tokens[1])))
+    {
+        nextState.linenumber = parseInt(tokens[1]);
+        return nextState;
+    }
+
+    nextState.error = true;
+    nextState.errorCode = "Invalid Parameter"
+    return nextState;
+}
+
+function BRGTCommand(nextState: ProgramState, state: ProgramState, tokens: string[]) : ProgramState {
+    if(tokens.length === 5 && state.state[tokens[1]] && state.state[tokens[2]] && !isNaN(parseInt(tokens[3])) && !isNaN(parseInt(tokens[4])))
+    {
+
+        if(parseInt(state.state[tokens[1]]) > parseInt(state.state[tokens[2]]))
+        {
+            nextState.linenumber = parseInt(tokens[3]) - 1;
+        } else {
+            nextState.linenumber = parseInt(tokens[4]) - 1;
+        }
+        return nextState;
+    }
+
+    nextState.error = true;
+    nextState.errorCode = "Invalid Parameter"
+    return nextState;
+}
+
+function BRLTCommand(nextState: ProgramState, state: ProgramState, tokens: string[]) : ProgramState {
+    if(tokens.length === 5 && state.state[tokens[1]] && state.state[tokens[2]] && !isNaN(parseInt(tokens[3])) && !isNaN(parseInt(tokens[4])))
+    {
+
+        if(parseInt(state.state[tokens[1]]) < parseInt(state.state[tokens[2]]))
+        {
+            nextState.linenumber = parseInt(tokens[3]) - 1;
+        } else {
+            nextState.linenumber = parseInt(tokens[4]) - 1;
+        }
+        return nextState;
+    }
+
+    nextState.error = true;
+    nextState.errorCode = "Invalid Parameter"
+    return nextState;
+}
+
+function BRLTEQCommand(nextState: ProgramState, state: ProgramState, tokens: string[]) : ProgramState {
+    if(tokens.length === 5 && state.state[tokens[1]] && state.state[tokens[2]] && !isNaN(parseInt(tokens[3])) && !isNaN(parseInt(tokens[4])))
+    {
+
+        if(parseInt(state.state[tokens[1]]) <= parseInt(state.state[tokens[2]]))
+        {
+            nextState.linenumber = parseInt(tokens[3]) - 1;
+        } else {
+            nextState.linenumber = parseInt(tokens[4]) - 1;
+        }
+        return nextState;
+    }
+
+    nextState.error = true;
+    nextState.errorCode = "Invalid Parameter"
+    return nextState;
+}
+
+function BRGTEQCommand(nextState: ProgramState, state: ProgramState, tokens: string[]) : ProgramState {
+    if(tokens.length === 5 && state.state[tokens[1]] && state.state[tokens[2]] && !isNaN(parseInt(tokens[3])) && !isNaN(parseInt(tokens[4])))
+    {
+
+        if(parseInt(state.state[tokens[1]]) >= parseInt(state.state[tokens[2]]))
+        {
+            nextState.linenumber = parseInt(tokens[3]) - 1;
+        } else {
+            nextState.linenumber = parseInt(tokens[4]) - 1;
+        }
+        return nextState;
+    }
+
+    nextState.error = true;
+    nextState.errorCode = "Invalid Parameter"
+    return nextState;
+}
+
+function BREQCommand(nextState: ProgramState, state: ProgramState, tokens: string[]) : ProgramState {
+    if(tokens.length === 5 && state.state[tokens[1]] && state.state[tokens[2]] && !isNaN(parseInt(tokens[3])) && !isNaN(parseInt(tokens[4])))
+    {
+        if(state.state[tokens[1]] === state.state[tokens[2]])
+        {
+            nextState.linenumber = parseInt(tokens[3]) - 1;
+            console.log("Line Number:" , nextState.linenumber)
+        } else {
+            nextState.linenumber = parseInt(tokens[4]) - 1;
+        }
+        return nextState;
+    }
+
+    nextState.error = true;
+    nextState.errorCode = "Invalid Parameter"
+    return nextState;
+}
+
+function EMPTY(nextState: ProgramState, state: ProgramState, tokens: string[]) : ProgramState {
+    return nextState;
+}
+
 
 const Commands: {[key: string] : (nextState: ProgramState, state: ProgramState, tokens: string[]) => ProgramState } = {
     "var": varCommand,
     "add": addCommand,
-    "sub": subCommand
+    "sub": subCommand,
+    "mult": multCommand,
+    "div": divCommand,
+    "goto": GOTOCommand,
+    "brgt": BRGTCommand,
+    ">": BRGTCommand,
+    "breq": BREQCommand,
+    "==": BREQCommand,
+    "brlt": BRLTCommand,
+    "<": BRLTCommand,
+    "brlteq": BRLTEQCommand,
+    "<=": BRLTEQCommand,
+    "brgteq": BRGTEQCommand,
+    ">=": BRGTEQCommand,
+    "": EMPTY,
 
 }
 
